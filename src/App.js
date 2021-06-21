f9a81fa2d1ba1d5f0b5528758892d9117b1cd375
subhadeepchatterjee1999 <subhadeepchatt99erjee@gmail.com>
2021-06-21 22:13:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import {useSelector, useDispatch } from "react-redux"
+import {incNumber, decNumber} from "./actions/index"
+ function App() {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="col">
+          <h1 className="text-secondary">Increment/Decrement</h1>
+        </div>
+
+        <div className="col">
+          <a title="Decrement" className="btn btn-secondary text-white" onClick={() => dispatch(decNumber())  }>-</a>
+          <input type="text" name="quantity" value={myState} className="text-center"/>
+          <a title="Increment" className="btn btn-secondary text-white" onClick={() => dispatch(incNumber())  }>+</a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/reducers/upDown.js
@@ -0,0 +1,9 @@
+const inintialState = 0;
+const changeTheNumber = (state=inintialState, action ) => {
+switch (action.type){
+case "INCREMENT": return state + 1;
+case "DECREMENT": return state-1;
+default: return state;
+}
+}
+export default changeTheNumber;
